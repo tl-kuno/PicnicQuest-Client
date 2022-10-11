774b64fb5462c14f0b1583656f4ec3975a4544f1
Taylor Kuno <kunot@oregonstate.edu>
2022-10-11 18:52:18 -0500

    class vs classname

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <section className="new-container with-title">
-        <h3 className="title">Picnic Quest Test UI</h3>
-        <button type="button" className="nes-btn is-success">
+      <section class="new-container with-title">
+        <h3 class="title">Picnic Quest Test UI</h3>
+        <button type="button" class="nes-btn is-success">
           "Click here"
           ::after
         </button>
