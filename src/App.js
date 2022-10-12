a7d998c4e7eb442aebe4b8e5a0bf0eb51bfa3cc9
Taylor Kuno <kunot@oregonstate.edu>
2022-10-12 08:45:41 -0500

    section to div

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import "nes.css/css/nes.min.css";
 function App() {
   return (
     <div className="App">
-      <section className="new-container with-title">
+      <div className="new-container with-title">
         <p className="title">Picnic Quest Test UI</p>
         <button type="button" className="nes-btn is-success">Click here</button>
-      </section>
+      </div>
     </div>
   );
 }
