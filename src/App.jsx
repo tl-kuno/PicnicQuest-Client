b64fb5a1f8c2690c25657d2928eb8c3f1eb271d0
Taylor Kuno <kunot@oregonstate.edu>
2022-10-12 13:24:30 -0500

    ready to be prepped for api demo

--- src/App.jsx
+++ src/App.jsx
@@ -32,13 +32,11 @@ function App() {
     }
   };
 
-  console.log(data);
-
   return (
     <div className="App">
       <div className="nes-container with-title">
         <p className="title">Picnic Quest Test UI</p>
-        <button onclick={handleClick} type="button" className="nes-btn is-success">Click here</button>
+        <button onClick={handleClick} type="button" className="nes-btn is-success">Click here</button>
       </div>
     </div>
   );
