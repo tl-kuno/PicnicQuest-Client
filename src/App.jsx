e3eeb82a3bef2a3f99b7094c6da58b719a381418
Taylor Kuno <kunot@oregonstate.edu>
2022-10-12 12:37:32 -0500

    prepare to test python server

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import './App.css';
+import React from 'react';
+import axios from 'axios';
+import "nes.css/css/nes.min.css";
+
+function App() {
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
+  const [err, setErr] = useState('');
+
+  const handleClick = async () => {
+    setIsLoading(true);
+    try {
+      const {data} = await axios.post(
+        'https://http://tlkuno.pythonanywhere.com/',
+        {command: 'sample command'},
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+          },
+        },
+      );
+
+      console.log(JSON.stringify(data, null, 4));
+
+      setData(data);
+    } catch (err) {
+      setErr(err.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="nes-container with-title">
+        <p className="title">Picnic Quest Test UI</p>
+        <button type="button" className="nes-btn is-success">Click here</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
